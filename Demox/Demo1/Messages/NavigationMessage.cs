bc8b677cd953322d8e83c3716ea5b0d7d75d184b
Michael Ponti <michael.ponti@live.ca>
2016-05-25 23:20:48 -0700

    added place holder project for demo

---
+++ Demox/Demo1/Messages/NavigationMessage.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Messages
+{
+	public class NavigationMessageArgs
+	{
+		public string PageToken { get; private set; } = null;
+		public object NavParameters { get; private set; } = null;
+
+		public NavigationMessageArgs(string pageToken, object navParameters = null)
+		{
+			PageToken = pageToken;
+			NavParameters = navParameters;
+		}
+	}
+
+
+	public class NavigationMessage : PubSubEvent<NavigationMessageArgs>
+	{
+	}
+}
---
+++ Demox/Demo1/ViewModels/AppViewModelBase.cs
@@ -0,0 +1,62 @@
+﻿using Demo1.Services;
+using Prism.Events;
+using Prism.Windows.AppModel;
+using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.ViewModels
+{
+	public abstract class AppViewModelBase : ViewModelBase
+	{
+		public IEventAggregator MessageBus { get; private set; } = null;
+		public IResourceLoader ResourceLoader { get; private set; } = null;
+		public INavigationService NavigationService { get; private set; }
+		public ITodoStorageService TodoStorageService { get; private set; }
+
+
+		public AppViewModelBase(IEventAggregator messageBus, IResourceLoader resourceLoader, INavigationService navigationService, ITodoStorageService storageService)
+		{
+			MessageBus = messageBus;
+			ResourceLoader = resourceLoader;
+			NavigationService = navigationService;
+			TodoStorageService = storageService;
+		}
+
+
+		private Messages.NavigationMessage _navigateMessage = null;
+		protected void NavigateToPage(string pageToken, object navParams = null)
+		{
+			if (_navigateMessage == null)
+				_navigateMessage = MessageBus.GetEvent<Messages.NavigationMessage>();
+
+			NavigationService.Navigate(pageToken, navParams);
+			_navigateMessage.Publish(new Messages.NavigationMessageArgs(pageToken, navParams));
+		}
+
+
+		private bool _isActive = false;
+		public bool IsActive
+		{
+			get { return _isActive; }
+			set { SetProperty<bool>(ref _isActive, value); }
+		}
+
+
+		//private Messages.UpdateNavigationMessage _updateNavigationMessage = null;
+
+		//public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+		//{
+		//	base.OnNavigatedTo(e, viewModelState);
+		//	string pageToken = e.SourcePageType.Name;
+		//	pageToken = pageToken.Substring(0, pageToken.Length - ("Page").Length);
+		//	if (_updateNavigationMessage == null)
+		//		_updateNavigationMessage = MessageBus.GetEvent<Messages.UpdateNavigationMessage>();
+		//	_updateNavigationMessage.Publish(pageToken);
+		//}
+	}
+}
